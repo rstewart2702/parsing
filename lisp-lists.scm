8370977fe70f08070e8831d41c85d5a46a43df6b
Richard S. Stewart <rstewart2702@gmail.com>
2023-10-03 16:34:00 -0500

    Further thinking:  trying to parse sums, simply.
    Need to further think through the nature of the resulting
    parse trees and if top-down parsing will do what is required.

--- lisp-lists.scm
+++ lisp-lists.scm
@@ -2,12 +2,21 @@
   (lambda (x)
     (or (number? x) (symbol? x))) )
 
+(define sumop?
+  (lambda (x)
+    (and (symbol? x) (or (eq? x '+) (eq? x '-)))))
+
+;; ====================================
+
+;; "general lisp lists"
+;; <GT> ::= <A> | <L>
 (define general-term?
   (lambda (t)
     (or
      (atom? t)
      (general-list? t))))
 
+;; <L> ::= '() | <GT> . <L>
 (define general-list?
   (lambda (t)
     (or
@@ -17,12 +26,17 @@
       (general-list? (cdr t)) ) ) ) )
 
 
+;; ====================================
+
+;; "flat" lists, list-of-atoms:
+;; <FT> ::= <A> | <LA>
 (define flat-term?
   (lambda (t)
     (or
      (atom? t)
      (list-of-atoms? t))) )
 
+;; <LA> ::= '() | <A> . <LA>
 (define list-of-atoms?
   (lambda (t)
     (or
@@ -32,9 +46,41 @@
       (list-of-atoms? (cdr t))) ) ) )
 
   
+;; ====================================
+
+;; An attempt at a "flat sums" kind of language/grammar:
+;; FS ::= <A> | <SS>
 (define flat-sum-parse
-  (lambda (ast s)
-    (let ((first (car s))
-	  (second (if (null? (cdr s)) '() (cadr s)))
-	  (remaining (cddr s)) )
+  (lambda (s)
+    (if (atom? s) (cons s '())
+        (let ((first (car s))
+              (remaining (if (null? (cdr s)) '() (cdr s))) )
+          (cond
+           ((atom? first) first)
+           ;; assumption:  it's a list-structure, otherwise:
+           (#t (simple-sum-parse s) )
+           )
+          )
+        )
+    )
+  )
+
+;; N.B. trying to use Lisp-style list structure here,
+;; the parentheses are NOT symbols in the input language.
+;; <SS> ::= '() | <FS> . ( <+-> . <SS> ) 
+(define simple-sum-parse
+  (lambda (s)
+    (if (null? s) '()
+        (let* ((first (car s))
+               (second
+                (if (null? (cdr s)) '() ;; this is an ERROR
+                    (if (sumop? (cadr s)) (cadr s)
+                        '() ;; this is an ERROR
+                        )))
+               (remaining (cddr s))
+               (fr (flat-sum-parse first))
+               (ptail (simple-sum-parse remaining)) )
+          (cons
+           second
+           (cons fr ptail))) ) ) )
       
